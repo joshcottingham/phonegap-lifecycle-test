bc2cd7184394c511867cc95377601852955d391f
joshcottingham <cotj1_14@uni.worc.ac.uk>
2015-10-22 16:28:30 +0100

    ddcd
    xsqxxasx

--- www/js/index.js
+++ www/js/index.js
@@ -1,41 +1,8 @@
-var paused_count =0;
-var resumed_count = 0;
-var launched_count = 0;
+var key = "pen";
+var value = "black";
 
-    function onLoad() {
-        document.addEventListener("deviceready", onDeviceReady, false);
-		console.log("device ready");
-    }
-	
-	function updateDisplay() {
-		$("#launched").text("Application launched: " + launched_count);
-		$("#resumed").text("Application paused: " + paused_count);
-		$("#paused").text("Application resumed: " + resumed_count);
-	}
+window.localStorage.setItem(key, value);
 
+var key = "pen";
+var value = window.localStorage.getItem(key);
 
-    // device APIs are available
-    //
-    function onDeviceReady() {
-		alert("device ready");
-        
-		document.addEventListener("resume", onResume, false);
-		document.addEventListener("pause", onPause, false);
-		
-		launched_count++;
-		updateDisplay();
-    }
-
-    // Handle the pause event
-    //
-    function onPause() {
-		alert("pause");
-		paused_count++;
-		updateDisplay();
-    }
-	
-	function onResume() {
-		alert("resume");
-		resumed_count++;
-		updateDisplay();
-    }
