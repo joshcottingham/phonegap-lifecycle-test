c454b730988bc4315575f28a12537e8fc0832173
joshcottingham <cotj1_14@uni.worc.ac.uk>
2015-10-22 17:21:21 +0100

    4
    4

--- www/js/index.js
+++ www/js/index.js
@@ -7,3 +7,24 @@ var key = "pen";
 var value = window.localStorage.getItem(key);
 
 alert(value);
+
+
+var simpsons = { 
+	"firstName": "Homer",
+	"lastName": "Simpson",
+     "address": 
+{
+	"streetAddress": "742 Evergreen Terrace",
+	"city": "Springfield",
+	"state": "?"
+} }
+
+console.log (simpsons.firstName + "is from" + simpsons.address.city);
+
+var homerString = JSON.stringify(simpsons); 
+
+window.localStorage.setItem(“address”, homerString);
+
+var getSimpson = window.localStorage.getItem(“address”); 
+
+var simpsonJSON = JSON.parse(getSimpson);
